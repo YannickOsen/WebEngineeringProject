1efa779a7fc0f608ba70ef06e755e4a4f58dc144
Tatjana0705 <hechttatjana94@web.de>
2019-07-05 17:45:25 +0200

    Connected Spring Security with custom login screen

--- QASystem/src/main/java/project/qasystem/persistence/model/SecurityControll.java
+++ QASystem/src/main/java/project/qasystem/persistence/model/SecurityControll.java
@@ -1,15 +1,16 @@
 package project.qasystem.persistence.model;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+        import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @Configuration
 public class SecurityControll implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("welcome");
-        registry.addViewController("/login").setViewName("testLogin");
+        registry.addViewController("/login").setViewName("signin");
+        registry.addViewController("/registration").setViewName("register");
         registry.addViewController("/home").setViewName("testLogout");
     }
 }
--- QASystem/src/main/java/project/qasystem/persistence/model/WebSecurityConfig.java
+++ QASystem/src/main/java/project/qasystem/persistence/model/WebSecurityConfig.java
@@ -1,23 +1,45 @@
 package project.qasystem.persistence.model;
 
+        import org.springframework.beans.factory.annotation.Autowired;
         import org.springframework.context.annotation.Bean;
         import org.springframework.context.annotation.Configuration;
+        import org.springframework.security.authentication.AuthenticationProvider;
+        import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
         import org.springframework.security.config.annotation.web.builders.HttpSecurity;
         import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
         import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
         import org.springframework.security.core.userdetails.User;
         import org.springframework.security.core.userdetails.UserDetails;
         import org.springframework.security.core.userdetails.UserDetailsService;
+        import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
         import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
-public class UserAuthentication extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+/*    @Autowired
+    UserDetailsService userDetailsService;*/
+
+    /**
+     * Method to authenticate a user by getting the user details from data source
+     * @return
+     */
+/*    @Bean
+    public AuthenticationProvider authenticate() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return provider;
+    }*/
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .antMatchers( "/").permitAll()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/registration").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -25,16 +47,16 @@ public class UserAuthentication extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
-                .permitAll();
+                .permitAll()
+                .clearAuthentication(true);
     }
-
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
         UserDetails user =
                 User.withDefaultPasswordEncoder()
                         .username("user")
-                        .password("password")
+                        .password("user")
                         .roles("USER")
                         .build();
         return new InMemoryUserDetailsManager(user);
