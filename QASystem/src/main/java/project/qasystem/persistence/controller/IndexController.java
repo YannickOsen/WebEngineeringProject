37cb3ba59696e8c30868de2e62db91eff3e0f1a1
hollerbe <benedikt.holler@gmail.com>
2019-07-14 21:57:49 +0200

    Jar executable

--- QASystem/src/main/java/project/qasystem/persistence/controller/IndexController.java
+++ QASystem/src/main/java/project/qasystem/persistence/controller/IndexController.java
@@ -1,9 +1,21 @@
 package project.qasystem.persistence.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.security.Principal;
 
 public class IndexController {
+
+    @ModelAttribute("userIsLoggedIn")
+    public String userIsLoggedIn(Principal principal) {
+        if(principal.getName() != null) {
+            return "true";
+        }
+        return "false";
+    }
 
     @RequestMapping(value = {"/login", "/"})
     public String login() {
